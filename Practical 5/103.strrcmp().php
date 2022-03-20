67611101143944f42ad71dc27f1ddf8179782c62
Vinayak <vinayaksawant859@gmail.com>
2022-03-20 12:45:01 +0530

    Practical 5 commit

---
+++ Practical 5/103.strrcmp().php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 84: strcmp()</title>
+</head>
+
+<body>
+    <?php
+    echo strcmp("Hello world!", "Hello world!");
+    echo strcmp("world!", "Hello PHP!");
+    echo strcmp("Hello PHP!", "world!");
+    ?>
+</body>
+
+</html>
